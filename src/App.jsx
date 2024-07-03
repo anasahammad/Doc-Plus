306a77e44f0a567731961e91b744c7845eb7ad91
Anas Ahammad <anasahammad2002@gmail.com>
2024-07-03 16:04:04 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/shared/Navbar'
+import Home from './pages/Home/Home'
 
 function App() {
-  
-
   return (
-    <>
-      
-      <h1 className='text-7xl'>Hello Bangaldesh</h1>  
-    </>
+    <main>
+      <Navbar/>
+      <Home/>
+    </main>
   )
 }
 
